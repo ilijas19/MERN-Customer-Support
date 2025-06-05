284a6843dcbe2c5c700ad31ac78257b2d099032a
ilijas19 <ilijagocic19@gmail.com>
2025-06-05 14:26:37 +0200

    test

--- backend/app.js
+++ backend/app.js
@@ -70,7 +70,7 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(`/files{/*path}`, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
