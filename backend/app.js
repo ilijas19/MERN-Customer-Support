f71ff4092a032b86da0fc3a82e612a15af5db6dc
ilijas19 <ilijagocic19@gmail.com>
2025-06-05 13:40:31 +0200

    fix

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,7 @@ app.use("/api/v1/user", userRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/(.*)/", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
