d96542905071e40a142b8df20279e44bae603c1b
ilijas19 <ilijagocic19@gmail.com>
2025-06-05 14:17:53 +0200

    fix

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -24,7 +24,6 @@ export const attachCookiesToResponse = ({ res, user, refreshToken }) => {
     secure: process.env.NODE_ENV === "production",
     maxAge: oneHour,
     sameSite: "Strict",
-    path: "/",
   });
 
   res.cookie("refreshToken", refreshTokenJwt, {
@@ -33,6 +32,5 @@ export const attachCookiesToResponse = ({ res, user, refreshToken }) => {
     secure: process.env.NODE_ENV === "production",
     maxAge: oneWeek,
     sameSite: "Strict",
-    path: "/",
   });
 };
